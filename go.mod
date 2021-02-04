6592885bf01e0205f5acf0bb3d30fda375cb0905
Francesco Cartier <francesco.cartier@testudosrl.com>
2021-02-04 10:24:28 +0100

    implementata Create

--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.15
 
 require (
 	github.com/99designs/gqlgen v0.13.0
-	github.com/jackc/pgx/v4 v4.10.1 // indirect
-	github.com/lib/pq v1.9.0
-	github.com/stretchr/testify v1.5.1 // indirect
+	github.com/google/uuid v1.2.0
+	github.com/jackc/pgx/v4 v4.10.1
+	github.com/lib/pq v1.9.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/vektah/gqlparser/v2 v2.1.0
 )
--- postgres/books_service.go
+++ postgres/books_service.go
@@ -1,11 +1,13 @@
 package postgres
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	"log"
 
-	// importo il driver
-	_ "github.com/lib/pq"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 // BooksService struct mi mette a disposizione le dipendenze di cui ho ibisogno per implementare l'interfaccia
@@ -21,7 +23,7 @@ type BooksService struct {
 func (t *BooksService) Init() error {
 	dbConnectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", t.DbHost, t.DbPort, t.DbUserName, t.DbPassword, t.DbName)
 
-	conn, err := sql.Open("postgres", dbConnectionString)
+	conn, err := sql.Open("pgx", dbConnectionString)
 
 	if err != nil {
 		return err
@@ -38,14 +40,44 @@ func (t *BooksService) Init() error {
 
 // Create metodo per implementare l'interfaccia BookService
 func (t *BooksService) Create(title string, author string) (*string, error) {
-	var id int
-	var createdAt string
-	query := `INSERT INTO books (title, author) VALUES ($1, $2) RETURNING id, created_at`
-	err := db.Conn.QueryRow(query, item.Name, item.Description).Scan(&id, &createdAt)
+	insertSQL := "insert into books(id, title, author) values ($1, $2, $3)"
+
+	ctx := context.Background()
+	dbPool := t.getConnection()
+	defer dbPool.Close()
+
+	tx, err := dbPool.Begin(ctx)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	item.ID = id
-	item.CreatedAt = createdAt
-	return nil
+
+	id := uuid.New()
+	idStr := id.String()
+
+	_, err = tx.Exec(ctx, insertSQL, idStr, title, author)
+	if err != nil {
+		log.Println("ERROR: Could not save the Book item due to the error:", err)
+		rollbackErr := tx.Rollback(ctx)
+		log.Fatal("ERROR: Transaction rollback failed due to the error: ", rollbackErr)
+		return nil, err
+	}
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &idStr, nil
+}
+
+func (t *BooksService) getConnection() *pgxpool.Pool {
+	dbPool, err := pgxpool.Connect(context.Background(), t.getDBConnectionString())
+	if err != nil {
+		log.Fatal(err)
+	}
+	return dbPool
+}
+
+func (t *BooksService) getDBConnectionString() string {
+	return fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", t.DbUserName, t.DbPassword, t.DbHost, t.DbName)
 }
